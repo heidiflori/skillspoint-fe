dd572fae67457163845ea8fbf7bc65b5a8760dd7
andreivremaroiu <andreivremaroiu225@gmail.com>
2023-06-13 21:12:45 +0300

    login-register route doesnt work

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,66 +1,101 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import Button from '@mui/material/Button'
-import Box from '@mui/material/Box'
+import { BrowserRouter as Routes, Route, Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import { Container, TextField } from '@mui/material';
 import Register from './Register';
 
-
-
-function checkUsername() {
+function checkUsername(event) {
+    event.preventDefault();
     const DUMMY_USERNAME = 'vrema';
-
-    const username = document.querySelector('#username_textfield');
-    if (!(username.value === DUMMY_USERNAME)) {
-        username.value = '';
+    const username = document.querySelector('#username_textfield').value;
+    if (!(username === DUMMY_USERNAME)) {
+        document.querySelector('#username_textfield').value = '';
         return false;
     } else {
-        username.value = '';
+        document.querySelector('#username_textfield').value = '';
         return true;
     }
 }
 
-function checkPassword() {
+function checkPassword(event) {
+    event.preventDefault();
     const DUMMY_PASSWORD = 'parola';
-
-    const password = document.querySelector('#password_textfield');
-    if (!(password.value === DUMMY_PASSWORD)) {
-        password.value = '';
+    const password = document.querySelector('#password_textfield').value;
+    if (!(password === DUMMY_PASSWORD)) {
+        document.querySelector('#password_textfield').value = '';
         return false;
     } else {
-        password.value = '';
+        document.querySelector('#password_textfield').value = '';
         return true;
     }
 }
 
-function checkCredentials() {
-    if (checkPassword() && checkUsername()) {
-        alert("Login successful");
+function checkCredentials(event) {
+    event.preventDefault();
+    if (checkPassword(event) && checkUsername(event)) {
+        alert('Login successful');
     } else {
         alert('Wrong username and/or password!');
     }
 }
 
 function Login() {
+    // const checkCredentials = () => {
+    //   // Add your logic here to check the credentials
+    // };
+  
     return (
-        <Container sx={{mt:5, p:0}}>
-            <Container sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', p: 'auto', m: 'auto', alignItems: 'center', border: "1px solid black", width:'50%'}}>
-                <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-                    <TextField sx={{ p: 1, m: 1 }} required id="username_textfield" label="Username" placeholder='Enter your username. e.g: firstname.lastname' />
-                    <TextField sx={{ p: 1, m: 1 }} required id='password_textfield' label="Password" placeholder='Enter your password' type='password' />
-                </Box>
-
-                <Box>
-                    <Link to="/"><Button sx={{ p: 1, m: 1 }} variant='contained' onClick={checkCredentials}>Log in</Button></Link>
-                    <Link to="/register"><Button sx={{ p: 1, m: 1 }} variant='contained'>Register</Button></Link>
-                </Box>
-            </Container>
-            <Routes>
-                <Route path='/'></Route>
-                <Route path='/register' element={<Register />}></Route>
-            </Routes>
-        </Container>
-    )
-}
+      <Routes>
+        <Route path="/" element={<Container
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            padding: 'auto',
+            margin: 'auto',
+            alignItems: 'center',
+            border: '1px solid black',
+            width: '50%',
+          }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+            <TextField
+              sx={{ padding: 1, margin: 1 }}
+              required
+              id="username_textfield"
+              label="Username"
+              placeholder="Enter your username. e.g: firstname.lastname"
+            />
+            <TextField
+              sx={{ padding: 1, margin: 1 }}
+              required
+              id="password_textfield"
+              label="Password"
+              placeholder="Enter your password"
+              type="password"
+            />
+          </Box>
+          <Box>
+            <Link to="/">
+              <Button
+                sx={{ padding: 1, margin: 1 }}
+                variant="contained"
+                onClick={checkCredentials}
+              >
+                Log in
+              </Button>
+            </Link>
+            <Link to="/register">
+              <Button sx={{ padding: 1, margin: 1 }} variant="contained">
+                Register
+              </Button>
+            </Link>
+          </Box>
+        </Container>}/>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    );
+  }
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,15 +1,84 @@
-import React from "react";
+import { Box, Button, Container, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
-    return (
-        <div>
-            <div>
-                register page
-            </div>
-        </div>
-        
-        
-    )
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("Username register:", username, password);
+
+    setUsername("");
+    setPassword("");
+
+    navigate("/login");
+  };
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "50px",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "300px",
+          marginBottom: "20px",
+        }}
+      >
+        <TextField
+          sx={{ marginBottom: "10px" }}
+          required
+          id="username_textfield"
+          label="Username"
+          value={username}
+          onChange={handleUsernameChange}
+          placeholder="Enter your username"
+        />
+        <TextField
+          sx={{ marginBottom: "10px" }}
+          required
+          id="password_textfield"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="Enter your Password"
+        />
+      </Box>
+
+      <Box>
+        <Button
+          sx={{ marginRight: "10px" }}
+          variant="contained"
+          onClick={handleSubmit}
+        >
+          Register
+        </Button>
+        <Button variant="contained" onClick={() => navigate("/login")}>
+          Go to Login
+        </Button>
+      </Box>
+    </Container>
+  );
 }
 
 export default Register;
