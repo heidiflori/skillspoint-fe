8eebeb4aa125222ac58b57c19ccd99b8b9425156
heidi <floriheidi06@gmail.com>
2023-06-23 22:15:22 +0300

    fixed login by adding auth and routing to home page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import { useNavigate } from "react-router-dom";
+import { useState, useRef, useContext } from "react";
 import { Container, TextField } from '@mui/material';
+import AuthContext from "../store/auth-context";
+import apiUrl from '../apiConfig';
 
+/*
 function checkUsername(event) {
   event.preventDefault();
   const DUMMY_USERNAME = 'vrema';
@@ -42,23 +46,84 @@ function checkCredentials(event) {
     return false;
   }
 }
+*/
 
 function Login() {
   const navigate = useNavigate();
+  // const history = useHistory();
+
+  const authCtx = useContext(AuthContext);
+
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
 
   function handleRegisterClick() {
-    let path = '/register';
-    navigate(path);
+    navigate('/register');
   }
 
-  function handleLoginClick(event) {
-    if (checkCredentials(event)) {
-      let path = "/home";
-      navigate(path);
-    } else {
-      alert('Invalid username/password');
-    }
-  }
+  // function handleLoginClick(event) {
+  //   if (checkCredentials(event)) {
+  //     let path = "/home";
+  //     navigate(path);
+  //   } else {
+  //     alert('Invalid username/password');
+  //   }
+  // }
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const handleLoginClick = (event) => {
+    event.preventDefault();
+
+    const enteredUsername = usernameInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // optional: add validation
+
+    setIsLoading(true);
+    
+    const url = `${apiUrl}/api/auth/signin`;
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        username: enteredUsername,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = "Authentication failed!";
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          throw new Error(errorMessage);
+        });
+      }
+    }).then(data => {
+      const expiresIn = 500;
+      const expirationTime = new Date(new Date().getTime() + (expiresIn * 1000));
+      console.log(expirationTime);
+      authCtx.login(data.accessToken, expirationTime.toISOString());
+      // history.replace('/');
+      navigate('/home');
+    }).catch(err => {
+      alert(err.message);
+    });
+  };
 
   return (
     <Container sx={{
@@ -79,6 +144,7 @@ function Login() {
           id="username_textfield"
           label="Username"
           placeholder="Enter your username. e.g: firstname.lastname"
+          inputRef={usernameInputRef}
         />
         <TextField
           sx={{ padding: 1, margin: 1 }}
@@ -87,6 +153,7 @@ function Login() {
           label="Password"
           placeholder="Enter your password"
           type="password"
+          inputRef={passwordInputRef}
         />
       </Box>
       <Box>
